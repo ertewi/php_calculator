47d1af681fa41936f75f22718174b479a8e76d24
ertewi <nikita28984@gmail.com>
2023-10-14 01:09:46 +0700

    basic design

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+	<table>
+		<tr>
+			<th colspan="4" class="field">
+				<form action="index.php">
+					<input type="text">
+				</form>
+			</th>
+		</tr>
+		<tr>
+			<td>C</td>
+			<td>&divide;</td>
+			<td>&times;</td>
+			<td>&#9003;</td>
+		</tr>
+		<tr>
+			<td>7</td>
+			<td>8</td>
+			<td>9</td>
+			<td>-</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td>5</td>
+			<td>6</td>
+			<td>+</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td>2</td>
+			<td>3</td>
+			<td rowspan="2">=</td>
+		</tr>
+		<tr>
+			<td>%</td>
+			<td>0</td>
+			<td>,</td>
+		</tr>
+		<tr></tr>
+		<tr></tr>
+	</table>
+</body>
+</html>
