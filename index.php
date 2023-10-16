109888d7ebaa15ff6fd21c13cc9717a89d3e3625
ertewi <nikita28984@gmail.com>
2023-10-16 18:37:38 +0700

    rework

--- index.php
+++ index.php
@@ -7,45 +7,53 @@
 	<link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
-	<table>
-		<tr>
-			<th colspan="4" class="field">
-				<form action="index.php">
-					<input type="text">
-				</form>
-			</th>
-		</tr>
-		<tr>
-			<td>C</td>
-			<td>&divide;</td>
-			<td>&times;</td>
-			<td>&#9003;</td>
-		</tr>
-		<tr>
-			<td>7</td>
-			<td>8</td>
-			<td>9</td>
-			<td>-</td>
-		</tr>
-		<tr>
-			<td>4</td>
-			<td>5</td>
-			<td>6</td>
-			<td>+</td>
-		</tr>
-		<tr>
-			<td>1</td>
-			<td>2</td>
-			<td>3</td>
-			<td rowspan="2">=</td>
-		</tr>
-		<tr>
-			<td>%</td>
-			<td>0</td>
-			<td>,</td>
-		</tr>
-		<tr></tr>
-		<tr></tr>
-	</table>
+	<form action="<?=$_SERVER["SCRIPT_NAME"]?>">
+		<input type="text" name="x">
+		<select name="operator">
+			<option value="+">+</option>
+			<option value="-">-</option>
+			<option value="*">*</option>
+			<option value="/">/</option>
+		</select>
+		<input type="text" name="y">
+		<input type="submit" value="calculate">
+	</form>
+	<?php
+		if(!isset($_REQUEST['x']) || !isset($_REQUEST['operator']) || !isset($_REQUEST['y'])) {
+			echo "<p>Enter operation</p>";
+		} elseif(!is_numeric($_REQUEST['x']) || !is_numeric($_REQUEST['y'])) {
+			echo "<p>Enter correct numbers</p>";
+		} else {
+			$x = $_REQUEST['x'];
+			$y = $_REQUEST['y'];
+			switch($_REQUEST['operator']) {
+				case '+':
+					$result = $x + $y;
+					break;
+				case '-':
+					$result = $x - $y;
+					break;
+				case '*':
+					$result = $x * $y;
+					break;
+				case '/':
+					$result = $x / $y;
+					break;
+				default:
+					echo "<p>Wrong operation</p>";
+			}
+			if(isset($result)) {
+				echo "<p>Solution: " . $result . "</p>";
+				$record = "<p>" . $x . " " . $_REQUEST['operator'] . " " . $y . " = " . $result . "</p>";
+				session_start();
+				$_SESSION['logs'][] = $record;
+				echo "History: \n";
+				foreach(array_reverse($_SESSION['logs']) as $log) {
+					echo "<p>" . $log . "</p>\n";
+				}
+			}
+
+		}
+	?>
 </body>
 </html>
